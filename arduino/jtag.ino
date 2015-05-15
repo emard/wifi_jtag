9f6048b6a15b0f3c41823fd7283e1e1690f1b4b1
davor <davor@lipa.lan>
2015-05-15 11:56:02 +0200

    Arduino/C implementation

--- arduino/jtag.ino
+++ arduino/jtag.ino
@@ -6,6 +6,13 @@
 
 #include <ESP8266WiFi.h>
 
+// cross reference gpio to nodemcu lua pin numbering
+// esp gpio:     0, 2,15,13,12,14,16
+// lua nodemcu:  3, 4, 8, 7, 6, 5, 0
+
+// GPIO pin assignment (e.g. 0 is GPIO0)
+enum { TDO=2, TDI=14, TCK=12, TMS=13, TRST=15, SRST=16 };
+
 const char* ssid = "ssid";
 const char* password = "password";
 
@@ -13,13 +20,52 @@ const char* password = "password";
 // specify the port to listen on as an argument
 WiFiServer server(3335);
 
+void jtag_on(void)
+{
+  Serial.println("jtag on");
+  pinMode(TDO, INPUT);
+  pinMode(TDI, OUTPUT);
+  pinMode(TCK, OUTPUT);
+  pinMode(TMS, OUTPUT);
+  pinMode(TRST, OUTPUT);
+  pinMode(SRST, OUTPUT);
+}
+
+void jtag_off(void)
+{
+  Serial.println("jtag off");
+  pinMode(TDO, INPUT);
+  pinMode(TDI, INPUT);
+  pinMode(TCK, INPUT);
+  pinMode(TMS, INPUT);
+  pinMode(TRST, INPUT);
+  pinMode(SRST, INPUT);
+}
+
+uint8_t jtag_read(void)
+{
+  return digitalRead(TDO);
+}
+
+void jtag_write(uint8_t tck_tms_tdi)
+{
+  digitalWrite(TDI, tck_tms_tdi & 1 ? HIGH : LOW);
+  digitalWrite(TMS, tck_tms_tdi & 2 ? HIGH : LOW);
+  digitalWrite(TCK, tck_tms_tdi & 4 ? HIGH : LOW);
+}
+
+void jtag_reset(uint8_t trst_srst)
+{
+  digitalWrite(SRST, trst_srst & 1 ? HIGH : LOW);
+  digitalWrite(TRST, trst_srst & 2 ? HIGH : LOW);
+}
+
 void setup() {
   Serial.begin(115200);
   delay(10);
 
-  // prepare GPIO2
-  pinMode(2, OUTPUT);
-  digitalWrite(2, 0);
+  // prepare GPIO
+  jtag_off();
   
   // Connect to WiFi network
   Serial.println();
@@ -49,21 +95,49 @@ void loop() {
   WiFiClient client = server.available();
   if(client)
   {
-    // Wait until the client sends some data
-    Serial.println("new client");
+    // Serial.println("new client");
+    jtag_on();
     while(client.connected())
     {
+      // loop infinitely until incoming data are available
       if(client.available())
       {
         char c = client.read();
         // Serial.write(c); 
-        client.write(c); // seems very slow
+        switch(c)
+        {
+          case '0':
+          case '1':
+          case '2':
+          case '3':
+          case '4':
+          case '5':
+          case '6':
+          case '7':
+            jtag_write(c & 7);
+            break;
+          case 'R':
+            client.write('0'+jtag_read()); // seems very slow switching from rx to tx
+            break;
+          case 'r':
+          case 's':
+          case 't':
+          case 'u':
+            jtag_reset((c-'r') & 3);
+            break;
+          case 'B':
+            Serial.println("blink on");
+            break;
+          case 'b':
+            Serial.println("blink off");
+            break;
+        }
       }
       // yield();
     }
     delay(1);
     client.stop();
-    Serial.println("client disonnected");
+    // Serial.println("client disonnected");
+    jtag_off();
   }
-  
 }
