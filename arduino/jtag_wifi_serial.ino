fad01ea3f546ae61db8dfe1310cdc4cde9754220
Emard <vordah@gmail.com>
2015-05-22 18:07:27 +0200

    rewritten using WiFiTelnetToSerial as base, seems even more stable

--- arduino/jtag_wifi_serial.ino
+++ arduino/jtag_wifi_serial.ino
@@ -1,6 +1,8 @@
-/*
- *  Arduino OpenOCD remote_bitbang WIFI-JTAG server for ESP8266
- *  LICENSE=GPL
+/*  
+ *  JTAG-WIFI with TCP-SERIAL by Emard
+ *
+ *  OpenOCD compatible remote_bitbang WIFI-JTAG server
+ *  for ESP8266 Arduino
  *
  *  OpenOCD interface:
  *
@@ -19,9 +21,35 @@
  *
  *  send file to tcp
  *  socat -u FILE:blink.cpp.hex TCP:xilinx.lan:3335
- */
+*/
+
+/* 
+  based on
+  WiFiTelnetToSerial - Example Transparent UART to Telnet Server for esp8266
+  by Hristo Gochkov
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 
 #include <ESP8266WiFi.h>
+
+
+const char* ssid = "ssid";
+const char* password = "password";
+
 
 // cross reference gpio to nodemcu lua pin numbering
 // esp gpio:     0, 2, 4, 5,15,13,12,14,16
@@ -35,36 +63,26 @@ enum { TDO=12, TDI=14, TCK=4, TMS=5, TRST=0, SRST=2, LED=16 };
 
 enum { MODE_JTAG=0, MODE_SERIAL=1 };
 
-const char* ssid = "ssid";
-const char* password = "password";
+uint8_t mode = MODE_JTAG; // input parser mode JTAG (remote bitbang)
 
 //how many clients should be able to telnet to this ESP8266
 #define MAX_SRV_CLIENTS 1
-
-// specify TCP port to listen on as an argument
-WiFiServer server(3335);
-WiFiClient clients[MAX_SRV_CLIENTS];
-
-// 0: disabled watchdog because ESP watchdog is not working
-// 1: enable watchdog
-#define WATCHDOG 0
 
 // *** serial port settings ***
 #define BAUDRATE 115200
 
 // 0: use standard  serial pins TX=GPIO1  RX=GPIO3
+//#define SERIAL_SWAP()
 // 1: use alternate serial pins TX=GPIO15 RX=GPIO13
-#define SERIAL_SWAP 1
+#define SERIAL_SWAP() Serial.swap()
 
 // 0: don't use additional TX
 // 1: use additional TX at GPIO2
 // this option currently doesn't work, leaving it 0
 #define TXD_GPIO2 0
 
-// serial RX buffer size
-// it must be buffered otherwise ESP8266 reboots
-// default 1024 bytes
-#define RXBUFLEN 1024
+WiFiServer server(3335);
+WiFiClient serverClients[MAX_SRV_CLIENTS];
 
 // led logic
 #define LED_ON LOW
@@ -119,53 +137,10 @@ void jtag_reset(uint8_t trst_srst)
   digitalWrite(TRST, trst_srst & 2 ? HIGH : LOW);
 }
 
-void setup() {
-  jtag_off();
-  #if WATCHDOG
-  ESP.wdtEnable(1024);
-  #endif
-  Serial.begin(BAUDRATE);
-  #if TXD_GPIO2
-  Serial1.begin(BAUDRATE);
-  #endif
-  #if SERIAL_SWAP
-  Serial.swap();
-  #endif
-  
-  pinMode(LED, OUTPUT);
-  
-  // Connect to WiFi network
-  //Serial.print("Connecting to ");
-  //Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    #if WATCHDOG
-    ESP.wdtFeed();
-    #endif
-    // blink when trying to connect
-    digitalWrite(LED, LED_ON);
-    delay(10);
-    digitalWrite(LED, LED_OFF);
-    delay(500);
-    //Serial.print(".");
-  }
-  
-  // Start the server
-  server.begin();
-  server.setNoDelay(true);
-  //Serial.println("Server started");
-
-  // Print the IP address
-  // Serial.println(WiFi.localIP());
-  pinMode(LED, INPUT);
-}
 
 void serial_break()
 {
-  #if SERIAL_SWAP
-  Serial.swap(); // second swap should un-swap
-  #endif
+  SERIAL_SWAP();
   Serial.end(); // shutdown serial port
   #if TXD_GPIO2
   // if we want to drive additional tx line
@@ -179,130 +154,130 @@ void serial_break()
   #endif
   delay(210); // at least 200ms we must wait for BREAK to take effect
   Serial.begin(BAUDRATE); // start port just before use
-  #if SERIAL_SWAP
-  Serial.swap(); // port started, break removed at GPIO15 (will now become serial TX)
-  #endif
+  // remove serial break either above or after swap
+  SERIAL_SWAP();
   #if TXD_GPIO2
   Serial1.begin(BAUDRATE); // port started, break removed at GPIO2 (will now become serial TX)
   #endif
   Serial.flush();
 }
 
-void loop() {
-  static uint8_t mode = MODE_JTAG; // start with JTAG remote_bitbang mode
-  uint8_t i;
-  uint8_t nconn = 0;
+void tcp_parser(WiFiClient *client)
+{
+  if(mode == MODE_JTAG)
+  {
+    char c = client->read();
+    switch(c)
+    {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+        jtag_write(c & 7); // it's the same as ((c-'0') & 7)
+        break;
+      case 'R':
+        client->write('0'+jtag_read());
+        break;
+      case 'r':
+      case 's':
+      case 't':
+      case 'u':
+        jtag_reset((c-'r') & 3);
+        break;
+      case 'B':
+        if(jtag_state == 0)
+          jtag_on();
+        digitalWrite(LED, LED_ON);
+        break;
+      case 'b':
+        digitalWrite(LED, LED_OFF);
+        break;
+      case '\0': // ctrl-@ (byte 0x00) sends serial BREAK
+        serial_break();
+        break;
+      case '\r': // press enter to tcp-serial mode
+        jtag_off();
+        mode = MODE_SERIAL;
+        break;
+      case 'Q':
+        jtag_off();
+        client->stop(); // disconnect client's TCP
+        break;
+    } /* end switch */
+  } // end mode == JTAG
+  else
+  {
+    // get data from the telnet client and push it to the UART
+    if(mode == MODE_SERIAL)
+      Serial.write(client->read());
+  }
+}
 
+void setup() {
+  Serial1.begin(BAUDRATE);
+  WiFi.begin(ssid, password);
+  Serial1.print("\nConnecting to "); Serial1.println(ssid);
+  uint8_t i = 0;
+  while (WiFi.status() != WL_CONNECTED && i++ < 20) delay(500);
+  if(i == 21){
+    Serial1.print("Could not connect to"); Serial1.println(ssid);
+    while(1) delay(500);
+  }
+  //start UART and the server
+  Serial.begin(BAUDRATE);
+  SERIAL_SWAP();
+  server.begin();
+  server.setNoDelay(true);
+  
+  Serial1.print("Ready! Use 'telnet ");
+  Serial1.print(WiFi.localIP());
+  Serial1.println(" 21' to connect");
+}
+
+void loop() {
+  uint8_t i;
   //check if there are any new clients
   if (server.hasClient()){
     for(i = 0; i < MAX_SRV_CLIENTS; i++){
       //find free/disconnected spot
-      if (!clients[i] || !clients[i].connected()){
-        if(clients[i]) clients[i].stop();
-        clients[i] = server.available();
-        // Serial1.print("New client: "); Serial1.print(i);
+      if (!serverClients[i] || !serverClients[i].connected()){
+        if(serverClients[i]) serverClients[i].stop();
+        serverClients[i] = server.available();
+        Serial1.print("New client: "); Serial1.print(i);
         mode = MODE_JTAG;
-        if(jtag_state == 0)
-          jtag_on();
         continue;
       }
     }
     //no free/disconnected spot so reject
-    WiFiClient client_reject = server.available();
-    client_reject.stop();
-    delay(1);
+    WiFiClient serverClient = server.available();
+    serverClient.stop();
   }
-  
-  #if WATCHDOG
-  ESP.wdtFeed();
-  #endif
-  
-  nconn = 0; // counts number of clients currently connected
   //check clients for data
-  for(i = 0; i < MAX_SRV_CLIENTS; i++)
-  {
-    if (clients[i] && clients[i].connected())
-    {
-        nconn++;
-        // client has connected. if JTAG interface was off, turn it on 
-        while(clients[i].available())
-        {
-          if(mode == MODE_JTAG)
-          {
-            char c = clients[i].read();
-            switch(c)
-            {
-              case '0':
-              case '1':
-              case '2':
-              case '3':
-              case '4':
-              case '5':
-              case '6':
-              case '7':
-                jtag_write(c & 7); // it's the same as ((c-'0') & 7)
-                break;
-              case 'R':
-                clients[i].write('0'+jtag_read());
-                break;
-              case 'r':
-              case 's':
-              case 't':
-              case 'u':
-                jtag_reset((c-'r') & 3);
-                break;
-              case 'B':
-                digitalWrite(LED, LED_ON);
-                break;
-              case 'b':
-                digitalWrite(LED, LED_OFF);
-                break;
-              case '\0': // ctrl-@ (byte 0x00) sends serial BREAK
-                serial_break();
-                break;
-              case '\r':
-                jtag_off();
-                mode = MODE_SERIAL;
-                break;
-              case 'Q':
-                clients[i].stop(); // disconnect client's TCP
-                break;
-            } /* end switch */
-          } // end mode == JTAG
-          else
-          {
-            // get data from the telnet client and push it to the UART
-            if(mode == MODE_SERIAL)
-              Serial.write(clients[i].read());
-          }
-         
-          #if WATCHDOG
-          ESP.wdtFeed();
-          #endif
-        }
+  for(i = 0; i < MAX_SRV_CLIENTS; i++){
+    if (serverClients[i] && serverClients[i].connected()){
+      if(serverClients[i].available()){
+        //get data from the telnet client and push it to the UART
+        // while(serverClients[i].available()) Serial.write(serverClients[i].read());
+        while(serverClients[i].available()) tcp_parser(&(serverClients[i]));
+      }
     }
   }
-
-  if(nconn == 0)
-  {
-    // all clients disconnected
-    // JTAG interface off
-    if(jtag_state != 0)
-      jtag_off();
-  }
-
   //check UART for data
   if(Serial.available()){
     size_t len = Serial.available();
     uint8_t sbuf[len];
     Serial.readBytes(sbuf, len);
     //push UART data to all connected telnet clients
-    if(mode == MODE_SERIAL)
-      for(i = 0; i < MAX_SRV_CLIENTS; i++){
-        if (clients[i] && clients[i].connected()){
-          clients[i].write(sbuf, len);
-          delay(1);
-        }
+    for(i = 0; i < MAX_SRV_CLIENTS; i++){
+      if (mode == MODE_SERIAL)
+      if (serverClients[i] && serverClients[i].connected()){
+        serverClients[i].write(sbuf, len);
+        delay(1);
       }
+    }
   }
 }
